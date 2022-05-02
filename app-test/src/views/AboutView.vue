617e712106c00a8c7ee5f6d1dd84278eeb369fb5
Adriano Ruiz Boldarini <ruiz@7cliques.com.br>
2022-05-01 22:42:26 -0300

    init

---
+++ app-test/src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt hic voluptate obcaecati, commodi illum voluptatem voluptas nisi quam dicta. Nulla molestias ad doloremque! Officiis, et. Illo iste id quod veritatis.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus fugiat recusandae placeat ducimus animi quibusdam ipsum aliquam cum molestiae ipsa? Eligendi saepe, natus minima corporis accusantium at rem officia sed!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi distinctio dolore porro voluptatem quo tenetur quaerat quae! Pariatur laudantium obcaecati rem aspernatur commodi maxime voluptate, et beatae est dolores esse.</p>
+  </div>
+</template>
---
+++ app-test/src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="about">
+    <v-list
+      flat
+    >
+   
+        <v-list-item 
+        v-for="task in tasks"
+        :key="task.id"
+        >
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{task.title}}</v-list-item-title>
+          
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+
+     
+      
+    </v-list>
+  </div>
+</template>
+
+<script>
+  
+
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        tasks:[
+          {
+            id:1,
+            title: 'Wake up'
+          },
+           {
+            id:2,
+            title: 'Get bananas'
+          },
+           {
+            id:3,
+            title: 'Eat bananas'
+          },
+        ]
+      }
+    }
+    
+  }
+</script>
